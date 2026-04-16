5d2bbd13d9b9ad552fd065131605ed68f6283bc2
Bryamn <morenobryan0215@gmail.com>
2026-04-15 19:56:51 -0600

    Update guardar.php

--- adopciones/guardar.php
+++ adopciones/guardar.php
@@ -1,5 +1,4 @@
 <?php
-include("conexion.php");
 include("Conexion.php");
 
 $nombre = $_POST['nombre'];
@@ -9,11 +8,10 @@ $tamano = $_POST['tamano'];
 $ubicacion = $_POST['ubicacion'];
 $estatus = $_POST['estatus'];
 
-$sql = "INSERT INTO animales (nombre, especie, edad, tamaño, ubicacion, estatus)
 $sql = "INSERT INTO animales (nombre, especie, edad, tamano, ubicacion, estatus)
 VALUES ('$nombre','$especie','$edad','$tamano','$ubicacion','$estatus')";
 
 $conexion->query($sql);
 
-header("Location: index.php");
+header("Location: mostrar.php");
 ?>
